8720c9c4b03800f634565382959f4e88040e179d
FrontendJoonik <fduran@joonik.com>
2018-02-28 14:55:36 -0500

    The 3 video

---
+++ swipe/src/Deck.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { View, Animated, PanResponder } from 'react-native';
+
+class Deck extends Component{
+	constructor(props){
+		super(props);
+		const PanResponder = PanResponder.create({
+				onStartShouldSetPanResponder: () => false,
+				onPanResponderMove: (event, gesture) => {
+					console.log(gesture);
+				},
+				onPanResponderRelease: () => {}
+		});
+		this.state = { panResponder };
+	}
+	renderCards(){
+		return this.props.data.map( item => { 
+			return this.props.renderCard(item) 
+		});
+	}
+  	render(){
+		return(
+			<View { ...this.state.panResponder.panHandlers }>
+				{this.renderCards()}
+			</View>
+		);
+		
+  	}
+}
+
+export default Deck;
